b43d9d45928826bf7a546020d2135752c4680edd
erik <tedderem@uw.edu>
2014-12-10 13:04:46 -0800

    updated some comments and made the layout of the program a little more clear.

--- CacheSim/src/model/Cache.java
+++ CacheSim/src/model/Cache.java
@@ -1,14 +1,19 @@
 package model;
 
 /**
+ * Model version of a Cache within the Cache Simulator program. Each cache consists of a size, 
+ * a latency (in number of cycles stalled on misses), and an array of CacheEntry values 
+ * depicting the cached instructions or values within this specific cache. 
  * 
- * @author Erik
- *
+ * @author Erik Tedder
  */
 public class Cache {
 
+	/** Size of the Cache. */
 	int myCacheSize;
+	/** Latency/Penalty for misses. */
 	int myLatency;
+	/** Stored values. */
 	CacheEntry[] myEntries;
 	
 	public Cache(final int theCacheSize, final int theLatency) {
@@ -17,7 +22,12 @@ public class Cache {
 		myEntries = new CacheEntry[theCacheSize];
 	}
 
-	
+	/**
+	 * The individual entries to a Cache's myEntries field. Each CacheEntry consists of a MESI
+	 * State and a tag. 
+	 * 
+	 * @author Erik Tedder	 
+	 */
 	public class CacheEntry {
 		char myMESIState;
 		int myTag;
